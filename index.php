3fd1a1894789fed96d5a1f3210bbefd24206b58a
gabrielaf27001 <62336681+gabrielaf27001@users.noreply.github.com>
2020-03-25 19:38:34 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<tittle>  </tittle>
+</head>
+<body>
+	<strong> bienvenidos </strong>
+<ul>
+	<li> <a href ="formulario.html"> Formulario </a></li>
+	<li> <a href = "#"> Módulo 2 </a> </li>
+	<li><a href = "#" >Módulo 3 </a> </li>
+</ul>
+</body>
+</html>
