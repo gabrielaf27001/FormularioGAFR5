450b636834a2d31f30411a9b08d53c676cd7aa9c
gabrielaf27001 <62336681+gabrielaf27001@users.noreply.github.com>
2020-04-06 09:32:32 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,50 @@
+<?php 
 
-<!DOCTYPE html>
+extract ($_REQUEST);
+$data=unserialize ($data);
+?><!DOCTYPE html>
 <html>
 <head>
-	<tittle>  </tittle>
-</head>
-<body>
-	<strong> bienvenidos </strong>
-<ul>
-	<li> <a href ="formulario.html"> Formulario </a></li>
-	<li> <a href = "#"> Módulo 2 </a> </li>
-	<li><a href = "#" >Módulo 3 </a> </li>
-</ul>
-</body>
-</html>
+	<title > Lista de Categorias </title>
+	<script type="text/javascript">
+		function eliminar(id){
+			if (confirm("seguro desea eliminar el registro?"))
+			{
+				window.location ="/PersonasControlador.php?operacion=eliminar&id_persona="+id;
+
+			}
+		}
+		</script>
+	</head>
+	<body>
+		<table align="center">
+			<a href="../index.php">Inicio</a>
+			<center> <a href ="PersonasControlador.php?operacion=registrar"> Registrar </a></center>
+			<tr> <th> Nro </th> <th>Nombres </th><th> Apellidos </th> <th> Cedula <th> <th> Opciones </th></th>
+				<?php $num=1;
+				for ($i =0; $i<$filas; $i++)
+				{
+					echo "<tr>";
+					?>
+					<td><?=$num?></td>
+					<?php for ($j =1; $j< $campos; $j++){
+
+						?>
+						<td><?=$data[$i][$j]?></td>
+                      <?php 
+					}
+					?>
+
+					<td><a href="PersonasControlador.php?operacion=modificar&id_persona=<?=$data[$i][0]?>">Modificar </a>
+						<a href="javascript:eliminar(<?=$data[$i][0]?>)">Eliminar</a>
+					</td>
+					<?php
+					$num++;
+				}
+				?>
+			</table>
+		</body>
+	</html>
+
+
+			
