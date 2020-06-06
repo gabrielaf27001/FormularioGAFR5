4d42392aa566d0d462c122beb598dd05f945d688
gabrielaf27001 <62336681+gabrielaf27001@users.noreply.github.com>
2020-06-06 16:38:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<ul>
 		<li> <a href="formulario_gabriela_ferray_28224085.html"> Formulario</a></li>
 		<li> <a href= "./personas/PersonasControlador.php?operacion=index">Personas </a></li>
-		<li> <a href="Vehiculos/mostrar.php">Herencia </a></li>
+		<li> <a href="vehiculos/mostrar.php">Herencia </a></li>
 
 
 	</ul> 
